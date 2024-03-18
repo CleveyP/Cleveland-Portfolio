7fe4372a0a6ad95cb738024c1b194db5cd032f06
CleveyP <clevelandplonsey@gmail.com>
2024-03-17 20:37:46 -0700

    add typewriting effect

--- client/src/app/components/projects/Projects.jsx
+++ client/src/app/components/projects/Projects.jsx
@@ -1,43 +1,42 @@
-    import projects from "./projects";
-    import React from "react";
-    import "./Projects.css";
-    import "../components.css";
-    const Projects = () => {
-        return (
-            <div id="projects" className="Projects section">
-                <h2>Projects</h2>
-                <div className="flex flex-row justify-between flex-wrap items-center border">
-                   {
-                          projects.map((project, index) => {
+import projects from "./projects";
+import React from "react";
+import "./Projects.css";
+import "../components.css";
+
+const Projects = () => {
+    return (
+        <div id="projects" className="Projects section">
+            <h2>Projects</h2>
+            <div className="flex flex-row justify-between flex-wrap items-center border">
+               {
+                      projects.map((project, index) => {
+                        return (
+                             <Project key={index} project={project} />
+                        );
+                      })
+               }
+            </div>
+        </div>
+    )
+}
+
+const Project = ({project}) => {
+    return (
+        <div className="project-card">
+            <a href={project.link}>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <div className="project-icons flex flex-row flex-wrap">
+                    {
+                        project.icons.map((icon, index) => {
                             return (
-                                 <Project key={index} project={project} />
-                            );
-                          })
-                   }
+                                <img className="icon" key={index} src={icon.src} title={icon.title} alt={icon.title} />
+                            )
+                        })
+                    }
                 </div>
-            </div>
-        )
-    }
-
-    const Project = ({project}) => {
-        return (
-            <div className="project-card">
-                <a href={project.link}>
-                    <h3>{project.title}</h3>
-                    <p>{project.description}</p>
-
-                    <div className="project-icons flex flex-row flex-wrap">
-                        {
-                            project.icons.map((icon, index) => {
-                                return (
-                                    <img className="icon" key={index} src={icon.src} title={icon.title} alt={icon.title} />
-                                )
-                            })
-                        }
-                    </div>
-                </a>
-            </div>
-        )
-    }
-
-    export default Projects;
+            </a>
+        </div>
+    )
+}
+export default Projects;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,12 @@
+'use client';
 import AboutMe from './components/aboutme/AboutMe';
 import NavBar from './components/navbar/NavBar';
 import Skills from './components/skills/Skills';
 import Projects from './components/projects/Projects.jsx';
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+
+
 
 export default function Home() {
   return (
@@ -9,7 +14,19 @@ export default function Home() {
         <NavBar />
         <hr></hr>
         <h1 className="text-6xl font-bold">Cleveland Plonsey,</h1>
-        <h2 className="text-4xl font-bold">Full Stack Developer</h2>
+        <h2 className="text-4xl font-bold">
+          {"<p>"}
+          <Typewriter
+            words={['Bachelor of Computer Science', 'Full Stack Developer', 'Job Seeker']}
+            loop={5}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+          {"</p>"}
+        </h2>
         <AboutMe />
         <Skills />
         <Projects />
